3e638b8824a7a174e375fab1edb62c035d6ed3c9
ruybragafilho <ruybragafilho@gmail.com>
2021-10-12 00:54:20 -0300

    Commit Final

--- todo.py
+++ todo.py
@@ -5,22 +5,23 @@ def firstChars(L):
     For instance:
     firstChars(['python', 'is', 'pythy']) == ['p', 'i', 'p']
     """
-    return None
+    return list( map( lambda x: x[0], L ) )
 
 def sum(L):
     """ Sums the elements in L """
-    return 0
+    return reduce( lambda acc, x: acc+x, L, 0 )
 
 def avg(L):
     """ Returns the average of the elements in L """
-    return 0
+    return sum(L) / reduce( lambda acc, x: acc+1, L, 0 )
+    
 
 def maxString(L):
     """ Retorna a maior string dentre as strings em L.
     Por exemplo: maxString(['python', 'is', 'pythy']) == 'python'
     Se houver empate, retorna a primeira string encontrada.
     """
-    return None
+    return reduce( lambda x, y: x if len(x) >= len(y) else y, L, "" )
 
 def add2Dict(D, N, S):
     """ Insere a string S na lista associada ao inteiro N dentro
@@ -38,11 +39,12 @@ def buildLenFreq(L):
     palavras daquele tamanho. Por exemplo:
     ins(['abc', 'xd', 'b', 'xxx']) = {1: ['b'], 2: ['xd'], 3: ['abc', 'xxx']}
     """
-    return None
+    return reduce( lambda D, x: add2Dict(D, len(x), x), L, {} )
 
 def incValue(D, N):
     """Esta funcao incrementa o valor associado a chave N dentro do dicionario
-    D. Por exemplo, se D = {1: 2, 2: 4, 3: 11}, entao
+    D. Por exemplo, se D = {1: 2, 2: 4, 3: 11}, entao 
+    incValue(D, 2) == {1: 2, 2: 5, 3: 11}
     Voce pode usar essa funcao para completar countFirsts
     """
     D[N] = D[N] + 1 if N in D else 1
@@ -54,11 +56,40 @@ def countFirsts(L):
     Note que essa funcao retorna um dicionario com cada caracter associada ao
     numero de strings que comecam com aquele caracter.
     """
-    return None
+    return reduce( lambda D, x: incValue(D, x[0]), L, {} )
 
 def mostCommonFirstChar(L):
     """ Retorna a letra mais comum entre as primeiras letras de strings em L.
     Por exemplo:
     mostCommonFirstChar(['python', 'is', 'pythy']) === 'p'
     """
-    return 'Z'
+    CF = countFirsts(L)
+    return reduce(lambda a, b: a[0] if CF[a[0]] >= CF[b[0]] else b[0], L, L[0])  
+
+
+
+L0 = [1, 2, 3, 4, 5, 6, 7]
+L1 = ["amora", "abacate", "beterraba", "cereja", "damasco", "ervilha"]
+
+print( "firstChars(L): ", firstChars(L1) )
+print( "sum: ", sum(L0) )
+print( "avg: ", avg(L0) )
+print( "maxString: ", maxString(L1) )
+
+D1 = {1: ['b'], 2: ['xd'], 3: ['abc']}
+print( "add2Dict: ", add2Dict(D1, 2, 'ww') )
+print( "add2Dict: ", add2Dict(D1, 7, 'aa') )
+
+L2 = ['abc', 'xd', 'b', 'xxx']
+print("\nL2: ", L2)
+print( "buildLenFreq: ", buildLenFreq(L2) )
+
+D2 = {1: 2, 2: 4, 3: 11}
+print( "incValue: ", incValue(D2, 2) )
+
+L3 = ['amar', 'o', 'estranho', 'deixa', 'confuso', 'este', 'coracao']
+print( "countFirsts: ", countFirsts(L3) )
+print( "mostCommonFirstChar: ", mostCommonFirstChar(L3) )
+
+
+
